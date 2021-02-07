f3c6699e96ff32c27dfef4b5d43ec12b4fd962d8
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-02-07 15:52:03 +0700

    Update api.php

--- api.php
+++ api.php
@@ -6,8 +6,8 @@ $ch = curl_init();
 curl_setopt( $ch, CURLOPT_URL, "https://api.myip.com" );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 
-$data = curl_exec( $ch); 
-curl_close( $ch); 
+$data = curl_exec( $ch ); 
+curl_close( $ch ); 
 
 echo $data; 
 ?>
