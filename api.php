abf4fe51f9699221356ade402d04acfefcca37f4
William Gabriel.S <76223089+menang22@users.noreply.github.com>
2021-02-07 14:15:36 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+// coded by Gabriel.S
+
+$ch = curl_init();
+
+curl_setopt( $ch, CURLOPT_URL, "https://api.myip.com" );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+
+$data = curl_exec( $ch); 
+curl_close( $ch); 
+
+echo $data; 
+?>
